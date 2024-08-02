2cd11af4770d4473fa594ce640b2a7446ac25b7c
RDD <RDD@localhost.localdomain>
2024-08-02 20:06:49 +0800

    revert

--- api/index.py
+++ api/index.py
@@ -5,10 +5,10 @@ from youtube_transcript_api.formatters import JSONFormatter
 import os
 
 # for videos without subtitles #
-import whisper
-from langdetect import detect
-from pytube import YouTube
-
+#import whisper
+#from langdetect import detect
+#from pytube import YouTube
+# for videos without subtitles #
 app = Flask(__name__)
 
 @app.route('/<video_id>', methods=['GET'])
@@ -30,29 +30,29 @@ def get_transcript(video_id):
         return send_file(filename, as_attachment=True)
 
     except Exception as e: # if video has no subtitles
-        try:
-            url = f"https://www.youtube.com/watch?v={video_id}"
-            yt = YouTube(url)
-            # Get the audio stream
-            audio_stream = yt.streams.filter(only_audio=True).first()
-            # Download the audio stream
-            audio_stream.download(output_paht="/tmp", filename=f"{video_id}.mp3")
+        # try:
+        #     url = f"https://www.youtube.com/watch?v={video_id}"
+        #     yt = YouTube(url)
+        #     # Get the audio stream
+        #     audio_stream = yt.streams.filter(only_audio=True).first()
+        #     # Download the audio stream
+        #     audio_stream.download(output_paht="/tmp", filename=f"{video_id}.mp3")
 
-            # Load the model
-            model = whisper.load_model("base")
-            result = model.transcribe(f"/tmp/{video_id}.mp3")
-            transcribed_text = result["text"]
+        #     # Load the model
+        #     model = whisper.load_model("base")
+        #     result = model.transcribe(f"/tmp/{video_id}.mp3")
+        #     transcribed_text = result["text"]
 
-            # Create and open a txt file with the text
-            filename = f"/tmp/" +f"{video_id}_whisper_tran.txt"
-            with open(filename, "w", encoding="utf-8") as file:
-                file.write(transcribed_text)
+        #     # Create and open a txt file with the text
+        #     filename = f"/tmp/" +f"{video_id}_whisper_tran.txt"
+        #     with open(filename, "w", encoding="utf-8") as file:
+        #         file.write(transcribed_text)
 
-            # Return the file
-            return send_file(filename, as_attachment=True)
+        #     # Return the file
+        #     return send_file(filename, as_attachment=True)
 
-        except Exception as n:
-            return jsonify({'error': str(e)}), 401
+        # except Exception as n:
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
